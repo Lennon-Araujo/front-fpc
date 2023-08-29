a8cab52e398a232fe91deb4494808064d64dde30
Lennon Araujo <lennon_hih@hotmail.com>
2023-08-29 17:31:19 -0300

    create Categories Layout

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,26 +1,26 @@
 import axios from "axios";
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-interface Category {
-  id: number;
-  name: string;
-  created_at: string;
-}
+// interface Category {
+//   id: number;
+//   name: string;
+//   created_at: string;
+// }
 
 export function Categories() {
-  const [categories, setCategories] = useState<Category[]>([])
+  // const [categories, setCategories] = useState<Category[]>([])
   const [categoryFormName, setCategoryFormName] = useState('')
 
-  async function populateCategories() {
-    const { data } = await axios.get('http://localhost:3000/category')
-    if(data.length > 0) {
-      setCategories([...data])
-    }
-  }
+  // async function populateCategories() {
+  //   const { data } = await axios.get('http://localhost:3000/category')
+  //   if(data.length > 0) {
+  //     setCategories([...data])
+  //   }
+  // }
 
-  useEffect(() => {
-    populateCategories()
-  }, [])
+  // useEffect(() => {
+  //   populateCategories()
+  // }, [])
 
   async function handleCreateCategory(event: FormEvent) {
     event.preventDefault()
@@ -29,7 +29,7 @@ export function Categories() {
     }
     const response = await axios.post('http://localhost:3000/category', payload)
     if(response.status === 201) {
-      populateCategories()
+      // populateCategories()
     }
   }
 
@@ -38,28 +38,25 @@ export function Categories() {
   }
 
   return (
-    <>
-      <h1 className="text-2xl">Categories</h1>
-      <form onSubmit={handleCreateCategory} className="flex gap-2 items-center justify-center">
+    <div className="w-4/5 p-7 mt-7 flex flex-col md:flex-row gap-5 md:gap-2 items-center justify-between bg-primary rounded-2xl"> 
+      <h1 className="text-center font-serif text-3xl text-secondary">Nova Categoria</h1>
+      <form onSubmit={handleCreateCategory} className="flex gap-4 items-center justify-center md:justify-end">
         <input
-          className="w-60 "
+          className="w-20 min-w-full md:w-40 border-none rounded p-2 bg-basic text-primary"
           type="text"
           name="Category"
           id="Category"
-          placeholder="Digite o nome da categoria"
+          placeholder="Ex: Moradia"
           value={categoryFormName} onChange={handleCategoryFormName}
         />
-        <button type="submit">Criar categoria</button>
+
+        <button
+          type="submit"
+          className="h-10 bg-secondary py-2 px-5 rounded font-serif text-2xl text-primary flex items-center"
+        >
+          Criar
+        </button>
       </form>
-      <div>
-        {
-          categories.map(category => {
-            return (
-              <li key={category.id}>{category.name}</li>
-            )
-          })
-        }
-      </div>
-    </>
+    </div>
   )
 }
