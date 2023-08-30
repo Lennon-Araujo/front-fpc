457cdbcffbe3ffd6761fc8a489847cf1f5a4bdb9
Lennon Araujo <lennon_hih@hotmail.com>
2023-08-29 23:13:53 -0300

    duplicate

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -92,7 +92,27 @@ export function Transactions() {
               <p className="text-sm text-basic">R$ 123,45</p>
             </div>
           </div>
+          <div className="flex gap-3 p-3 bg-primary w-56 h-auto rounded-2xl">
+            <div className="w-2/3 flex flex-col gap-1 text-left">
+              <span className="text-sm text-basic mb-1">Supermercado</span>
+              <p className="text-xs text-basic">Alimentação</p>
+              <p className="text-xs text-basic">Em: 11/08/2023</p>
+            </div>
+            <div className="w-full text-right flex flex-col gap-1">
+              <div className="text-xs text-basic flex justify-end gap-2">
+                <Pencil size={20} />
+                <Trash size={20} />
+              </div>
+              <div className="text-xs text-primary flex justify-end">
+                <span className="w-fit  bg-secondary rounded-full p-0.5">
+                  <Users size={20} />
+                </span>
+              </div>
+              <p className="text-sm text-basic">R$ 123,45</p>
+            </div>
+          </div>
         </div>
+        
         {/* <table className="rounded-2xl overflow-hidden w-full border-spacing-y-2">
           <caption className="h-8 pt-1 caption-bottom text-zinc-50 text-xs font-sans font-light rounded-b-3xl bg-basic/20 border-secondary border-t">
             <CheckCircle size={20} className="p-0.5 mb-0.5 mr-0.5 inline-block text-secondary" />
