b82eb4b5f39caa2133a96a2dcded29c879b3395b
Lennon Araujo <lennon_hih@hotmail.com>
2023-09-01 20:14:43 -0300

    fix responsivity

--- src/components/CreateTransactionModal.tsx
+++ src/components/CreateTransactionModal.tsx
@@ -58,20 +58,20 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
 
   return (
     <Modal
-      className="h-[75vh] w-[80vw] mx-auto my-[10vh] bg-primary rounded-2xl flex items-center justify-start flex-col overflow-hidden"
+      className="h-[80vh] w-[80vw] lg:w-[50vw] mx-auto my-[10vh] bg-primary rounded-2xl flex items-center justify-start flex-col overflow-hidden"
       overlayClassName="modal_overlay"
       isOpen={isOpen}
       onRequestClose={closeModal}
     >
       <header className="p-5 bg-primary w-full flex justify-between">
         <h2 className="font-serif text-secondary text-3xl">Nova Transação</h2>
-        <X onClick={closeModal} size={38} className="text-secondary hover:text-primary hover:bg-secondary transition cursor-pointer rounded-sm " />
+        <X onClick={closeModal} size={38} className="text-secondary hover:text-primary hover:bg-secondary transition cursor-pointer rounded-sm" />
       </header>
 
-      <div className="p-5 h-full w-full flex items-center justify-center bg-basic">
-        <form onSubmit={handleCreateTransaction} className="flex flex-col items-center gap-3 w-full max-w-sm">
-          <h2 className="w-full max-w-xs p-0.5 text-primary text-center text-lg">Preencha o formulário para criar uma nova transação</h2>
-          <div className="w-full max-w-xs p-0.5 flex flex-col gap-1">
+      <div className="px-5 h-full w-full flex items-start justify-center bg-basic overflow-y-scroll">
+        <form onSubmit={handleCreateTransaction} className="flex flex-col w-full max-w-sm py-3 md gap-3">
+          <h2 className="w-full p-0.5 text-primary text-center text-lg">Preencha o formulário para criar uma nova transação</h2>
+          <div className="w-full p-0.5 flex flex-col gap-1">
             <label htmlFor="name" className="text-primary">Transação</label>
             <input
               type="text"
@@ -85,8 +85,7 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
                 w-full
                 h-10
                 text-secondary
-                text-lg
-                font-serif
+                font-sans
                 truncate
                 placeholder:text-primary/30
                 ${transactionsFormData.name && 'bg-primary'}
@@ -96,7 +95,7 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
             />
           </div>
 
-          <div className="w-full max-w-xs p-0.5 flex flex-col gap-1">
+          <div className="w-full p-0.5 flex flex-col gap-1">
             <label htmlFor="datepicker" className="text-primary">Data da transação</label>
             <DatePicker
               selected={transactionsFormData.when}
@@ -111,15 +110,14 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
               w-full
               h-10
               text-primary
-              text-lg
-              font-serif
+              font-sans
               truncate
               "
             />
           </div>
 
-          <div className="w-full max-w-xs p-0.5 flex items-center justify-center gap-4">
-            <div className="w-full p-0.5 flex flex-col gap-1">
+          <div className="w-full p-0.5 flex items-center justify-between gap-4">
+            <div className="w-2/3 p-0.5 flex flex-col gap-1">
               <label htmlFor="cost" className="text-primary">Custo</label>
               <input
                 type="number"
@@ -135,8 +133,7 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
                 w-full
                 h-10
                 text-secondary
-                text-lg
-                font-serif
+                font-sans
                 placeholder:text-primary/30
                 shadow-sm
                 ${transactionsFormData.cost && 'bg-primary'}
@@ -144,20 +141,19 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
               />
             </div>
 
-            <div className="w-full p-0.5 flex gap-2 items-center pt-7">
+            <div className="w-full sm:w-1/2 p-0.5 flex pl-2 items-center pt-7">
               <input
                 type="checkbox"
                 name="shared"
                 id="shared"
                 checked={transactionsFormData.shared}
-                className="selection:bg-slate-500"
                 onChange={(event) => setTransactionsFormData((prevState) => ({...prevState, shared: event.target.checked}))}
               />
-              <label htmlFor="shared" className="text-primary text-xs">Este custo será compartilhado?</label>
+              <label htmlFor="shared" className="text-primary text-sm pl-3">Este custo será compartilhado?</label>
             </div>
           </div>
 
-          <div className="w-full max-w-xs p-0.5 flex flex-col gap-1">
+          <div className="w-full p-0.5 flex flex-col gap-1">
             <label htmlFor="categoryId" className="text-primary">Categoria</label>
             <select
               name="categoryId"
@@ -171,8 +167,7 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
                 w-full
                 h-10
                 text-secondary
-                text-lg
-                font-serif
+                font-sans
                 placeholder:text-primary/30
                 shadow-sm
                 ${transactionsFormData.categoryId && 'bg-primary'}`
@@ -189,14 +184,14 @@ export function CreateTransactionModal({isOpen, closeModal}: CreateTransactionMo
             disabled={!isDisabled}
             className="
               w-full
-              max-w-xs
+             
               p-3
               mt-7
               rounded
               bg-secondary
               text-primary
-              font-serif
-              text-xl
+              font-sans font-semibold
+              text-lg
               hover:bg-secondary/80
               hover:text-primary/80
               transition
