dd88ce7d4a91c5a77f4df627c5251d07d4df48ff
Lennon Araujo <lennon_hih@hotmail.com>
2024-01-11 20:31:09 -0300

    feat: error page and redirect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,10 @@ export function Header() {
   return (
     <header className="bg-primary w-full h-28 flex flex-col items-center justify-center">
       <div className="text-center font-serif text-6xl text-secondary mt-5">
-        Finance
+        <a href="/">
+          Finance
         <span className="block text-center font-handwriting text-3xl -mt-5 text-basic">Personal Control</span>
+        </a>
       </div>
     </header>
   )
--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -1,10 +1,16 @@
 
+import errorpage from "../assets/errorpage.svg"
+
 export function ErrorPage() {
 
 
   return (
     <div>
-      <h1>Ops, something went wrong!</h1>
+      <a className="w-auto p-10 mt-5 flex flex-col bg-primary rounded-2xl" href="/">
+        <h1 className="text-center font-mono text-lg text-secondary">Ops, something went wrong!</h1>
+        <p className="text-center font-mono text-md underline text-secondary">Please return to safety!</p>
+      <img src={errorpage} alt="a person crossing their arms in an X" />
+      </a>
     </div>
   )
 }
