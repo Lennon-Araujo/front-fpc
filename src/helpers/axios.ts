8118b8fac85bf411687ac1132ad8e2eece9dac6f
Lennon Araujo <lennon_hih@hotmail.com>
2024-01-17 23:48:26 -0300

    fix: baseURL

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: 'https://back-fpc.onrender.com'
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://back-fpc.onrender.com'
+  // baseURL: 'http://localhost:3333'
 })
