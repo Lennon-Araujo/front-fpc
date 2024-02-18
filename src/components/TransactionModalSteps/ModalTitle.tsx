c66ceccab3b6068b7695a497cde6c28ac053d267
Lennon Araujo <lennon_hih@hotmail.com>
2024-02-18 15:05:27 -0300

    fix: close modal on update transaction

--- src/components/TransactionModalSteps/ModalTitle.tsx
+++ src/components/TransactionModalSteps/ModalTitle.tsx
@@ -4,13 +4,13 @@ interface ModalTitlePropsType {
   handleFinishUpdatingTransaction: () => void;
 }
 
-export function ModalTitle(handleFinishUpdatingTransaction: ModalTitlePropsType) {
+export function ModalTitle({handleFinishUpdatingTransaction}: ModalTitlePropsType) {
   return (
     <header className="p-5 bg-primary w-full flex justify-between">
     <h2 className="font-serif text-secondary text-3xl">
       Atualizar Transação
     </h2>
-    <X onClick={() => handleFinishUpdatingTransaction} size={38} className="text-secondary hover:text-primary hover:bg-secondary transition cursor-pointer rounded-sm" />
+    <X onClick={handleFinishUpdatingTransaction} size={38} className="text-secondary hover:text-primary hover:bg-secondary transition cursor-pointer rounded-sm" />
   </header>
   )
 }
